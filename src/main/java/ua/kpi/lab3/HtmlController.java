809e36291d51128e50ee87beecd6c4df9003350a
Yevhenii Smyrnov <kalledat57@gmail.com>
2025-05-03 20:27:15 +0300

    init

---
+++ src/main/java/ua/kpi/lab3/HtmlController.java
@@ -0,0 +1,19 @@
+package ua.kpi.lab3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class HtmlController {
+
+    @GetMapping("")
+    public String index(Model model) {
+        model.addAttribute("time", LocalDateTime.now().withNano(0));
+        return "index";
+    }
+
+
+}
